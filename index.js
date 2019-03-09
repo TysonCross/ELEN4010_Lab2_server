6bd0a015266088acb1ea47dbaeca86b836fe1475
Tyson <tyson.cross@gmail.com>
2019-03-09 18:14:38 +0200

    added routing file

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-let path = require('path')
+// let path = require('path')
 let express = require('express')
 let app = express()
+let mainRouter = require('./mainRoutes')
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
-app.get('/about/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+app.use(mainRouter)
 
 app.listen(3000)
 console.log('Express server running on port 3000')

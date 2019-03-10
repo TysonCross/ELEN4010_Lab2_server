0ac49709d4618024d030185fa5adc2cf72b8b9b2
Tyson <tyson.cross@gmail.com>
2019-03-10 11:26:38 +0200

    Fixed routing path error

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-// let path = require('path')
 let express = require('express')
 let app = express()
 let mainRouter = require('./mainRoutes')
-
-app.use(mainRouter)
-
-let port = process.env.port || 80
+app.use('/', mainRouter)
+let port = process.env.port || 3000
 app.listen(port)
-
 console.log('Express server running on port', port)

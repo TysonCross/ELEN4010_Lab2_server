97e927dfb8fe2408281aa6a1e00002c87ce84b0d
Tyson <tyson.cross@gmail.com>
2019-03-10 11:34:01 +0200

    Corrected formatting and double checking code

--- mainRoutes.js
+++ mainRoutes.js
@@ -1,6 +1,7 @@
 let path = require('path')
 let express = require('express')
 let mainRouter = express.Router()
+
 mainRouter.get('/', function (req, res) {
     res.send('Hello World, I\'m Node.js')
 })
